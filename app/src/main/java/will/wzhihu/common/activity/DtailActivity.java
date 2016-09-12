1c56eb969239c470026d7194f53eda1c4cde9429
unknown <taoming@1verge.com>
2016-09-12 17:50:41 +0800

    add detail

---
+++ app/src/main/java/will/wzhihu/common/activity/DtailActivity.java
@@ -0,0 +1,20 @@
+package will.wzhihu.common.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import will.wzhihu.R;
+import will.wzhihu.common.activity.BaseActivity;
+import will.wzhihu.common.fragment.DetailFragment;
+
+/**
+ * Created by taoming on 2016/9/9.
+ */
+public class DtailActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_content, new DetailFragment()).commit();
+    }
+}
---
+++ app/src/main/java/will/wzhihu/common/fragment/DetailFragment.java
@@ -0,0 +1,19 @@
+package will.wzhihu.common.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by taoming on 2016/9/9.
+ */
+public class DetailFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+}

9a11093b76af287a0987821f559161d867583734
Deping Wen <deping@eryuapp.com>
2016-09-13 10:44:38 +0800

    change DetailActivity start method implement

--- app/src/main/java/will/wzhihu/detail/DetailActivity.java
+++ app/src/main/java/will/wzhihu/detail/DetailActivity.java
@@ -1,11 +1,11 @@
 package will.wzhihu.detail;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import will.wzhihu.R;
+import will.wzhihu.common.activity.ActivityStarter;
 import will.wzhihu.common.activity.BaseActivity;
 import will.wzhihu.main.model.Story;
 
@@ -16,9 +16,9 @@ public class DetailActivity extends BaseActivity {
     public static final String PARAMS_STORY = "params_story";
 
     public static void start(Story story, Activity activity) {
-        Intent intent = new Intent(activity, DetailActivity.class);
-        intent.putExtra(PARAMS_STORY, story);
-        activity.startActivity(intent);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(PARAMS_STORY, story);
+        new ActivityStarter(DetailActivity.class, bundle).start(activity);
     }
 
     @Override
